53a02be28c78b55aeaaee4321aa660bdd71667a5
JD Huntington <jdh@microsoft.com>
2019-10-14 16:27:37 -0700

    Rearrange files

--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import { exampleTheme } from "./exampleTheme";
-import { resolveTokens } from "./resolveTokens";
-
-console.log(
-  resolveTokens(exampleTheme, exampleTheme.components.Checkbox.tokens)
-);
